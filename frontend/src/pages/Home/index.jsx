c61d463a51d7055877102aead785c309623e11c7
josuenm <direct.josue@gmail.com>
2022-05-22 00:23:18 -0300

    Correção de erro gramátical na rota home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -4,14 +4,15 @@ import Prestation from "../../assets/images/presentation.png"
 import { Head } from "../../components/Head";
 
 export function Home() {
-    return(
+    return (
         <Container>
 
             <Head title="Home | JavaScript Notes" description="" />
 
             <div className="home__body">
                 <div className="home__body__title">
-                    <h2>Create notes easily and access when you wants in the cloud</h2>
+                    <h2>Create notes easily and access them whenever you want in the cloud</h2>
+
 
                     <p>
                         Now you have a place to keep your notes! With our service you can save your notes for free and access them whenever and wherever you want! Your notes will always be available and safely stored
